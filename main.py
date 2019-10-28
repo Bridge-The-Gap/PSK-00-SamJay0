8053fae319852193cabd4f5738f428b376af8ddd
Samjay0 <samja0.254@gmail.com>
2019-10-28 12:54:08 +0300

    week1 assignment

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import statistics
 print("Hello World")
+print("My name is Sam\nI am 20+ years of age. Young, right? "+"\N{slightly smiling face}")
+list1=[12, 4, 56, 17, 8, 99]
+print("The maximum number in this list : "+str(list1)+" is "+str(max(list1)))
+print("The mean: "+str(list1)+" is "+str(statistics.mean(list1)))
+print("A for Apple \nB for Boy\nC for Cow\n...\nZ for Zebra")
