6381f91ecb51bfde0b9d082c74ca8a6354288e80
Samjay0 <samja0.254@gmail.com>
2019-10-28 13:03:36 +0300

    implemented for loop

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ print("My name is Sam\nI am 20+ years of age. Young, right? "+"\N{slightly smili
 list1=[12, 4, 56, 17, 8, 99]
 print("The maximum number in this list : "+str(list1)+" is "+str(max(list1)))
 print("The mean: "+str(list1)+" is "+str(statistics.mean(list1)))
-print("A for Apple \nB for Boy\nC for Cow\n...\nZ for Zebra")
+myAlphabets = ['A for Apple','B for Boy','C for Cat','D for Dog','E for Elephant','F for Fish','G for Girl','H for Hen','I for Ice cream','J for Jug','K for Kettle','L for Lion','M for Man','N for Nurse','O for Ostrich','P for Puppy','Q for Queen','R for Rabbit','S for Ship','T for Tree','U for Umbrella','V for Van','W for Woman','X for X-mass tree','Y for Yatch','Z for Zebra']
+
+for item in myAlphabets:
+    print(item)
